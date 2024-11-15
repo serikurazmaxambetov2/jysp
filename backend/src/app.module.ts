1c6223e62f46efe86a289c08f4957e8fa3880f3b
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-15 14:34:23 +0500

    Базовый ресурс для session

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { ChannelModule } from './channel/channel.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './database/data.source';
 import { ChannelRelationModule } from './channel-relation/channel-relation.module';
+import { SessionModule } from './session/session.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSourceOptions),
     ChannelModule,
     ChannelRelationModule,
+    SessionModule,
   ],
 })
 export class AppModule {}
