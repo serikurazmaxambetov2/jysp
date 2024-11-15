7c401a959e759173120355104f0a6969a5cba3d9
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-15 06:01:54 +0500

    Базовый ресурс для channel

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ChannelModule } from './channel/channel.module';
 
 @Module({
-  imports: [],
+  imports: [ChannelModule],
 })
 export class AppModule {}
