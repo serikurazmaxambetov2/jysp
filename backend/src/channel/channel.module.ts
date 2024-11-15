0a0fdfd6f1ae48ace4304f8a4208c3205b70f62a
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-15 06:09:24 +0500

    Добавление Channel entity в module

--- backend/src/channel/channel.module.ts
+++ backend/src/channel/channel.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChannelService } from './channel.service';
 import { ChannelController } from './channel.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EChannel } from './channel.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([EChannel])],
   controllers: [ChannelController],
   providers: [ChannelService],
 })
