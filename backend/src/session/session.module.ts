22c98f401dfc73d14bc616a560572226da6bf7ab
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-16 10:06:15 +0500

    Добавление Session entity в module

--- backend/src/session/session.module.ts
+++ backend/src/session/session.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SessionService } from './session.service';
 import { SessionController } from './session.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ESession } from './session.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ESession])],
   controllers: [SessionController],
   providers: [SessionService],
 })
