7d21e25dd6e6a8ec78017a73563ba7d6bda955fe
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-16 06:23:51 +0500

    Добавление ChannelRelation entity в module

--- backend/src/channel-relation/channel-relation.module.ts
+++ backend/src/channel-relation/channel-relation.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChannelRelationService } from './channel-relation.service';
 import { ChannelRelationController } from './channel-relation.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EChannelRelation } from './channel-relation.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([EChannelRelation])],
   controllers: [ChannelRelationController],
   providers: [ChannelRelationService],
 })
