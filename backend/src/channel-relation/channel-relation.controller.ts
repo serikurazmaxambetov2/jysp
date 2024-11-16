37e2aa0f5c9299b9ed66cc9bc2368b84e1b0a7fd
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-16 07:31:01 +0500

    Логики для контроллера в channel_relation

--- backend/src/channel-relation/channel-relation.controller.ts
+++ backend/src/channel-relation/channel-relation.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Post,
+} from '@nestjs/common';
 import { ChannelRelationService } from './channel-relation.service';
+import { DChannelRelationCreate } from './dto/create.dto';
+import { DChannelRelationDelete } from './dto/delete.dto';
 
 @Controller('channel-relation')
 export class ChannelRelationController {
   constructor(private channelRelationService: ChannelRelationService) {}
+
+  @Post()
+  async create(@Body() dto: DChannelRelationCreate) {
+    const channelExists = await this.channelRelationService.checkExists(dto);
+    if (channelExists) throw new BadRequestException('ALREADY_EXISTS');
+
+    return await this.channelRelationService.create(dto);
+  }
+
+  @Delete()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Body() dto: DChannelRelationDelete) {
+    const result = await this.channelRelationService.delete(dto);
+
+    // Проверка что удаление произошло успешно
+    if (result.affected == 0) throw new NotFoundException('NOT_FOUND');
+  }
 }
