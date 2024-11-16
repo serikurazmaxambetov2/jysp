b5af68460921b5be1773b7097afd1bc4b70a9d3a
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-16 20:04:30 +0500

    Сервис для взаимодействия с backend

---
+++ listener/listener/services/api.py
@@ -0,0 +1,25 @@
+from typing import Any, Dict, List
+
+from aiohttp import ClientSession
+
+from listener import config
+
+
+class ApiService:
+    def __init__(self, base_url: str):
+        self.base_url = base_url
+
+    def _get_client(self):
+        return ClientSession(self.base_url)
+
+    async def get_relations(self) -> List[Dict[str, Any]]:
+        client = self._get_client()
+
+        async with client:
+            response = await client.get(f"/session/{config.USER_ID}/")
+            json_data = await response.json()
+
+            return json_data.get("listenRelations")
+
+
+api_service = ApiService(config.BACKEND_URL)
