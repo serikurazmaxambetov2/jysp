7176303055697d02e123aa2862a4c484f6719e89
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-16 07:46:18 +0500

    Исправлена ошибка: неправильный тип при валидаций

--- backend/src/channel-relation/dto/create.dto.ts
+++ backend/src/channel-relation/dto/create.dto.ts
@@ -1,13 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 import { ValidateNested } from 'class-validator';
 import { DChannelFindById } from 'src/channel/dto/find-by-id.dto';
 
 export class DChannelRelationCreate {
   @ApiProperty({ type: DChannelFindById })
   @ValidateNested()
+  @Type(() => DChannelFindById)
   fromChannel: DChannelFindById;
 
   @ApiProperty({ type: DChannelFindById })
   @ValidateNested()
+  @Type(() => DChannelFindById)
   toChannel: DChannelFindById;
 }
