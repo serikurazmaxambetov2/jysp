4ea9c283b6ca1906e8aa1fc0903f7f6c9bd37bb5
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-16 11:02:44 +0500

    Добавление TelegramUser entity в module

--- backend/src/telegram-user/telegram-user.module.ts
+++ backend/src/telegram-user/telegram-user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TelegramUserService } from './telegram-user.service';
 import { TelegramUserController } from './telegram-user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ETelegramUser } from './telegram-user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ETelegramUser])],
   controllers: [TelegramUserController],
   providers: [TelegramUserService],
 })
