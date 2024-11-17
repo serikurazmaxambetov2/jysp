a85a9026dc5b7bd85a4fbe252282da443921b333
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-17 12:14:55 +0500

    Добавлены поддерживаемые типы медиафайлов

--- listener/listener/config.py
+++ listener/listener/config.py
@@ -1,5 +1,7 @@
 import os
 
+from pyrogram.enums import MessageMediaType
+
 USER_ID = int(os.getenv("USER_ID", "0"))
 SESSION_STRING = os.getenv("SESSION_STRING", "")
 
@@ -8,3 +10,12 @@ BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:3000/")
 RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@localhost")
 PUBLISHER_QUEUE = os.getenv("PUBLISHER_QUEUE", "PUBLISHER")
 UNIFIER_QUEUE = os.getenv("UNIFIER_QUEUE", "UNIFIER")
+
+ALLOWED_MEDIA_TYPES = [
+    MessageMediaType.AUDIO,
+    MessageMediaType.PHOTO,
+    MessageMediaType.VIDEO,
+    MessageMediaType.DOCUMENT,
+    MessageMediaType.VIDEO_NOTE,
+    MessageMediaType.VOICE,
+]
