d30b88df667a94f2b7bf5e6c62551413dcde7d41
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-19 17:09:49 +0500

    Инструмент для извлечения json с ответа

--- unifier/unifier/utils/extract_unique_text.py
+++ unifier/unifier/utils/extract_unique_text.py
@@ -4,11 +4,15 @@ import re
 
 def extract_unique_text(text: str):
     """Получаем ответ в json формате и возвращаем его"""
-    if text.startswith("```"):
-        match = re.search(r"```json(.*?)```", text, re.DOTALL)
-        if not match:
-            return None
+    try:
+        if text.startswith("```"):
+            match = re.search(r"```json(.*?)```", text, re.DOTALL)
+            if not match:
+                return None
 
-        text = match.group(1).strip()
+            text = match.group(1).strip()
 
-    return json.loads(text)
+        unique_text = json.loads(text).get("unique")
+        return unique_text
+    except Exception:
+        return None
