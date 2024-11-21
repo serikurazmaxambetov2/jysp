dee96214c97642a72158f2a7c0e3ba0fd5bb7313
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-21 16:22:40 +0500

    Добавлен файл запуска

---
+++ publisher/main.py
@@ -0,0 +1,41 @@
+import asyncio
+import json
+import logging
+
+import aio_pika
+from aiogram import Bot
+
+from publisher import config
+from publisher.services import PublisherService, rmq_service
+
+# Настройка логирования на русском
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+
+async def on_message(msg: aio_pika.abc.AbstractIncomingMessage):
+    try:
+        bot = Bot(config.BOT_TOKEN)
+        body = json.loads(msg.body.decode())
+        publisher = PublisherService(body)
+
+        async with bot:
+            await publisher.publish(bot)
+
+        logger.info("Сообщение успешно опубликовано.")
+
+    except Exception as e:
+        logger.error(f"Ошибка при обработке сообщения: {e}")
+
+
+async def main():
+    try:
+        logger.info("Запуск потребления сообщений из очереди...")
+        await rmq_service.consume(config.PUBLISHER_QUEUE, on_message)
+
+    except Exception as e:
+        logger.error(f"Ошибка при запуске потребителя: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
