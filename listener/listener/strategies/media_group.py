ad11873184a7190e8e6336fafcc61e81393a69e5
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-23 11:07:11 +0500

    Исправлена ошибка: неверное удаление ключа media_group

--- listener/listener/strategies/media_group.py
+++ listener/listener/strategies/media_group.py
@@ -6,7 +6,7 @@ class MediaGroupStrategy(BaseStrategy):
         updated_data = data.copy()
 
         if data.get("media_group") and not relation.get("useMediaGroup"):
-            del data["media_group"]
-            del data["media_group_type"]
+            del updated_data["media_group"]
+            del updated_data["media_group_type"]
 
         return updated_data, queue
