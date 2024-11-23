78775951f41add427c627404bcfb8479b9779af1
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-23 09:54:49 +0500

    Миграция для настроек useAi, useMedia, useMediaGroup

---
+++ backend/src/database/migrations/1732337461608-channel_relation_options.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class ChannelRelationOptions1732337461608 implements MigrationInterface {
+  name = 'ChannelRelationOptions1732337461608';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "channel_relation" ADD "use_ai" boolean NOT NULL DEFAULT false`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "channel_relation" ADD "use_media" boolean NOT NULL DEFAULT true`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "channel_relation" ADD "use_media_group" boolean NOT NULL DEFAULT true`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "channel_relation" DROP COLUMN "use_ai"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "channel_relation" DROP COLUMN "use_media_group"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "channel_relation" DROP COLUMN "use_media"`,
+    );
+  }
+}
