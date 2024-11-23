7f95b6ea77294623e01e84d2dc4b839017d248dc
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-23 14:46:19 +0500

    Исправлена ошибка: неверное условие при добавлений форматирования

--- listener/listener/strategies/formatting.py
+++ listener/listener/strategies/formatting.py
@@ -6,7 +6,7 @@ class FormattingStrategy(BaseStrategy):
         updated_data = data.copy()
         del updated_data["markup_text"]
 
-        if relation.get("useFormatting"):
+        if relation.get("useFormatting") and "original_text" in updated_data:
             markup_text = data.get("markup_text")
             updated_data["original_text"] = markup_text
 
