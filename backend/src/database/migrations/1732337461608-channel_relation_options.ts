52033ecc20e879a5fe70a74dde1f172cab7cfbbb
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-23 14:17:21 +0500

    Миграция для настройки useFormatting

--- backend/src/database/migrations/1732337461608-channel_relation_options.ts
+++ backend/src/database/migrations/1732337461608-channel_relation_options.ts
@@ -5,6 +5,9 @@ export class ChannelRelationOptions1732337461608 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
+      `ALTER TABLE "channel_relation" ADD "use_formatting" boolean NOT NULL DEFAULT true`,
+    );
+    await queryRunner.query(
       `ALTER TABLE "channel_relation" ADD "use_ai" boolean NOT NULL DEFAULT false`,
     );
     await queryRunner.query(
@@ -16,6 +19,9 @@ export class ChannelRelationOptions1732337461608 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "channel_relation" DROP COLUMN "use_formatting"`,
+    );
     await queryRunner.query(
       `ALTER TABLE "channel_relation" DROP COLUMN "use_ai"`,
     );
