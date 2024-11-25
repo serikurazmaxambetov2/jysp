b9e677d30ee5deb10557ddb785276fc98d3eb613
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-25 12:31:13 +0500

    Добавлен markup_text для сохранения разметки

--- listener/listener/handlers/media.py
+++ listener/listener/handlers/media.py
@@ -10,10 +10,11 @@ class MediaHandler(MessageHandler):
         if msg.media:
             # Отправляем в бота чтобы он имел доступ к file_id медиа.
             logger.info("Копируем медиа в бота")
-            await msg.copy("olxuzsearchbot")
+            await msg.copy("jysp_bot")
 
             logger.info("Обрабатываем медиа сообщение")
             send_data["original_text"] = msg.caption
+            send_data["markup_text"] = msg.caption.html
             send_data["media_type"] = msg.media.value
             return send_data
         return await super().handle(msg, send_data)
