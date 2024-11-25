c17d586e872c1cdfe13eaf4703f828c1afc540ed
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-25 12:31:30 +0500

    Добавлены новые стратегий

--- listener/listener/strategies/__init__.py
+++ listener/listener/strategies/__init__.py
@@ -1,8 +1,16 @@
 from .ai import AIStrategy
 from .base import BaseStrategy
 from .composite import CompositeStrategy
+from .formatting import FormattingStrategy
 from .media import MediaStrategy
 from .media_group import MediaGroupStrategy
+from .stop_words import StopWordsStrategy
 
-strategies = [AIStrategy(), MediaStrategy(), MediaGroupStrategy()]
+strategies = [
+    AIStrategy(),
+    MediaStrategy(),
+    MediaGroupStrategy(),
+    FormattingStrategy(),
+    StopWordsStrategy(),
+]
 compositor = CompositeStrategy(strategies)
