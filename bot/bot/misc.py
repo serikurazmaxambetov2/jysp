c807760003301d17070fb72d1d14694aca4774a3
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-26 19:38:42 +0500

    Инициализация i18n

--- bot/bot/misc.py
+++ bot/bot/misc.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.base import DefaultKeyBuilder
 from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.utils.i18n import I18n
 from redis.asyncio import Redis
 
 from . import config
@@ -13,3 +14,6 @@ storage = RedisStorage(redis=redis, key_builder=key_builder)
 # Other
 bot = Bot(config.BOT_TOKEN)
 dp = Dispatcher(storage=storage)
+
+# I18n
+i18n = I18n(path="locales", default_locale="ru")
