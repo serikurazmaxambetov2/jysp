da557b9c32c70ee366d22bb9e14b78d653b16a11
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-26 19:40:05 +0500

    Добавление i18n middleware

--- bot/main.py
+++ bot/main.py
@@ -5,6 +5,7 @@ from aiogram_dialog import setup_dialogs
 
 from bot.dialogs import dialog_registry
 from bot.handlers import main_router
+from bot.middlewares import i18n_middleware
 from bot.misc import bot, dp
 
 
@@ -15,6 +16,10 @@ async def main():
     # Добавляем обработчики
     dp.include_router(main_router)
     dp.include_router(dialog_registry)
+
+    # I18n
+    dp.message.middleware(i18n_middleware)
+    dp.callback_query.middleware(i18n_middleware)
 
     # Запускаем polling
     await dp.start_polling(bot)
