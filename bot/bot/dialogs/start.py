890710955402969714c7610b6981acf66aad4331
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-26 18:21:29 +0500

    Добавлен getter для диалога

--- bot/bot/dialogs/start.py
+++ bot/bot/dialogs/start.py
@@ -1,9 +1,18 @@
-from aiogram_dialog import Dialog, Window
+from aiogram_dialog import Dialog, DialogManager, Window
 from aiogram_dialog.widgets.text import Const
 
 from ..keyboards import start_menu
 from ..states import MainSG
 
+
+async def getter(dialog_manager: DialogManager, **_):
+    middleware_data = dialog_manager.middleware_data
+    return {
+        "user": middleware_data.get("user"),
+    }
+
+
 start_dialog = Dialog(
-    Window(Const("dialogs.start.main"), start_menu, state=MainSG.START)
+    Window(Const("dialogs.start.main"), start_menu, state=MainSG.START),
+    getter=getter,
 )
