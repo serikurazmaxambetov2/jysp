b4d06eb0edb88a12e4330f8c585a2e6d269ace9c
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-26 20:00:03 +0500

    Добавлен getter для диалога

--- bot/bot/dialogs/start.py
+++ bot/bot/dialogs/start.py
@@ -1,9 +1,19 @@
-from aiogram_dialog import Dialog, Window
+from aiogram_dialog import Dialog, DialogManager, Window
 
 from ..keyboards import start_menu
 from ..states import MainSG
 from ..widgets import I18nFormat
 
+
+async def getter(dialog_manager: DialogManager, **_):
+    return dialog_manager.start_data
+
+
 start_dialog = Dialog(
-    Window(I18nFormat("dialogs.start.main"), start_menu, state=MainSG.START)
+    Window(
+        I18nFormat("dialogs.start.main"),
+        start_menu,
+        state=MainSG.START,
+        getter=getter,  # type: ignore
+    )
 )
