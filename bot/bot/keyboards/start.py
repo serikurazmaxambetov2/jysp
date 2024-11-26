ee06c2646a1ee90f3b69fc2638c66f3aec3385e4
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-26 20:37:52 +0500

    Button изменены для Start, чтобы переходить на другой state

--- bot/bot/keyboards/start.py
+++ bot/bot/keyboards/start.py
@@ -1,13 +1,23 @@
-from aiogram_dialog.widgets.kbd import Button, Group, Row, Url
+from aiogram_dialog.widgets.kbd import Button, Group, Row, Start, Url
 from aiogram_dialog.widgets.text import Const
+
+from bot.states.channel import ChannelSG
 
 from ..widgets import I18nFormat
 
 start_menu = Group(
     Url(I18nFormat("keyboards.start.instructions"), Const("https://t.me/")),
     Row(
-        Button(I18nFormat("keyboards.start.add_channel"), "add_channel"),
-        Button(I18nFormat("keyboards.start.my_channels"), "my_channels"),
+        Start(
+            I18nFormat("keyboards.start.add_channel"),
+            "add_channel",
+            state=ChannelSG.ADD,
+        ),
+        Start(
+            I18nFormat("keyboards.start.my_channels"),
+            "my_channels",
+            state=ChannelSG.LIST,
+        ),
     ),
     Row(
         Button(I18nFormat("keyboards.start.create_relation"), "create_relation"),
