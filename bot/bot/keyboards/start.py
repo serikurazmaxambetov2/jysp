42fb1ffc21f8b6c1326d475f1ca80cd3d5f09745
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-27 19:25:20 +0500

    Удалена кнопка добавление канала

--- bot/bot/keyboards/start.py
+++ bot/bot/keyboards/start.py
@@ -7,17 +7,10 @@ from ..widgets import I18nFormat
 
 start_menu = Group(
     Url(I18nFormat("keyboards.start.instructions"), Const("https://t.me/")),
-    Row(
-        Start(
-            I18nFormat("keyboards.start.add_channel"),
-            "add_channel",
-            state=ChannelSG.ADD,
-        ),
-        Start(
-            I18nFormat("keyboards.start.my_channels"),
-            "my_channels",
-            state=ChannelSG.LIST,
-        ),
+    Start(
+        I18nFormat("keyboards.start.my_channels"),
+        "my_channels",
+        state=ChannelSG.LIST,
     ),
     Row(
         Button(I18nFormat("keyboards.start.create_relation"), "create_relation"),
