02873124012750c710f685d5aa6b6460b30d9431
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-27 19:05:48 +0500

    Добавлен edit_mode при старте

--- bot/bot/handlers/start.py
+++ bot/bot/handlers/start.py
@@ -1,6 +1,7 @@
-from aiogram import Router, types
+from aiogram import Router
 from aiogram.filters import Command
-from aiogram_dialog import DialogManager
+from aiogram.types import Message
+from aiogram_dialog import DialogManager, ShowMode
 
 from ..middlewares import RegisterMiddleware
 from ..states import MainSG
@@ -10,6 +11,8 @@ start_router.message.middleware(RegisterMiddleware())
 
 
 @start_router.message(Command("start"))
-async def start_cmd(msg: types.Message, dialog_manager: DialogManager):
-    await msg.delete()
-    await dialog_manager.start(MainSG.START)
+async def start_cmd(_: Message, dialog_manager: DialogManager):
+    await dialog_manager.start(
+        MainSG.START,
+        show_mode=ShowMode.EDIT,
+    )
