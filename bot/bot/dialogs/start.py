abe8bfed4eb03ea6f2a9cda85d52aa915a4f957f
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-27 19:11:38 +0500

    Изменена логика для получения пользователя

--- bot/bot/dialogs/start.py
+++ bot/bot/dialogs/start.py
@@ -1,12 +1,15 @@
+from aiogram.types import User
 from aiogram_dialog import Dialog, DialogManager, Window
 
 from ..keyboards import start_menu
+from ..services import api_service
 from ..states import MainSG
 from ..widgets import I18nFormat
 
 
-async def getter(dialog_manager: DialogManager, **_):
-    return dialog_manager.start_data
+async def getter(dialog_manager: DialogManager, event_from_user: User, **_):
+    user = await api_service.get_user(event_from_user.id)
+    return {"user": user, "name": user.get("fullName")}
 
 
 start_dialog = Dialog(
